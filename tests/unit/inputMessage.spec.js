3674e50326cc9bbaba9e3d1917813b7dcd8caa97
Dymitr Snigurenko <dymitr@jedenslad.pl>
2021-09-01 17:17:09 +0200

    warning fix

---
+++ tests/unit/inputMessage.spec.js
@@ -0,0 +1,13 @@
+import { mount } from '@vue/test-utils'
+
+
+const App = {
+  template: `
+  <div> Hello </div>`
+}
+
+test('App', () => {
+  const wrapper = mount(App)
+  console.log(wrapper.html())
+})
+
